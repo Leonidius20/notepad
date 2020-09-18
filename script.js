e620fcddc37c038dc1ad53799fc2551e0effc1f8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-09-18 17:10:34 +0300

    improved note deleting

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 window.onload = showNoteList;
 
+// maybe we should take this variable outta here
+let currentNoteId = null;
+
 function saveCurrentNode() {
     let notes = JSON.parse(window.localStorage.getItem('notes')) || [];
-    notes[notes.length] = {
-        id: notes.length,
-        title: document.getElementById('titleBox').value,
-        text: document.getElementById('textBox').value,
-        date: 'currentDate',
-    };
-    console.log(JSON.stringify(notes));
+    if (currentNoteId === null) {
+        notes[notes.length] = {
+            id: notes.length,
+            title: document.getElementById('titleBox').value,
+            text: document.getElementById('textBox').value,
+            date: 'currentDate',
+        };
+    } else {
+        for (let i = 0, n = notes.length; i < n; i++) {
+            if (notes[i].id === currentNoteId) {
+                notes[i].title = document.getElementById('titleBox').value;
+                notes[i].text = document.getElementById('textBox').value;
+                notes[i].date = 'newDate';
+            }
+        }
+    }
+
     window.localStorage.setItem('notes', JSON.stringify(notes));
     showNoteList();
 }
@@ -28,19 +41,41 @@ function getNoteTitleCard(id, title, date) {
     div.appendChild(document.createElement('br'));
     div.appendChild(document.createTextNode(`${date}`));
 
+    const showButton = document.createElement('button');
+    showButton.innerHTML = 'show';
+    showButton.onclick = () => showNote(id);
+    div.appendChild(showButton);
+
     const delButton = document.createElement('button');
     delButton.innerHTML = 'del';
     delButton.onclick = () => deleteNote(id);
-
     div.appendChild(delButton);
 
     div.setAttribute('id', id);
+
     return div;
+}
+
+function getNote(id) {
+    let notes = JSON.parse(window.localStorage.getItem('notes')) || [];
+    return notes.find(note => note.id === id);
 }
 
 function deleteNote(id) {
     let notes = JSON.parse(window.localStorage.getItem('notes')) || [];
     notes = notes.filter(note => note.id !== id);
     window.localStorage.setItem('notes', JSON.stringify(notes));
+    if (id === currentNoteId) {
+        currentNoteId = null;
+        document.getElementById('titleBox').value = '';
+        document.getElementById('textBox').value = '';
+    }
     showNoteList();
+}
+
+function showNote(id) {
+    currentNoteId = id;
+    const note = getNote(id);
+    document.getElementById('titleBox').value = note.title;
+    document.getElementById('textBox').value = note.text;
 }
