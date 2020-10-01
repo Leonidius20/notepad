b18a34dcbc0497c94d657a7ca18460ce3149f02e
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-10-01 22:39:03 +0300

    added pwa capabilities (closes #3)

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const FILES_TO_CACHE = [
+    'index.html', 'main.css', 'script.js', 'note_dao.js', 'notes_list_item.html',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css', // bootstrap css
+    'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+    'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js',
+];
+
+const CACHE_NAME = 'cache';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    // TODO: Remove previous cached data from disk.
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    if (event.request.mode !== 'navigate') {
+        // Not a page navigation, bail.
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match(event.request);
+                    });
+            })
+    );
+});
