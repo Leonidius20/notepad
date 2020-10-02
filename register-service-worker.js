ad084dfc19660d1d5ae705ad37137025d861c414
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2020-10-02 12:23:31 +0300

    removed logging

--- register-service-worker.js
+++ register-service-worker.js
@@ -1,8 +1,5 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('service-worker.js')
-            .then((reg) => {
-                console.log('Service worker registered.', reg);
-            });
+        navigator.serviceWorker.register('service-worker.js').then(() => {});
     });
 }
--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ const CACHE_NAME = 'cache';
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('[ServiceWorker] Pre-caching offline page');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -23,7 +22,6 @@ self.addEventListener('activate', (event) => {
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
-                    console.log('[ServiceWorker] Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
